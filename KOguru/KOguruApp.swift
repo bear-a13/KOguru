1609a7bbfa39705f8b3df9adbb79f86b48af37b9
bear-a13 <bernardo.saraujo11@gmail.com>
2026-08-31 09:12:26 -0300

    Initial Commit

---
+++ KOguru/KOguruApp.swift
@@ -0,0 +1,17 @@
+//
+//  KOguruApp.swift
+//  KOguru
+//
+//  Created by Bernardo on 31/08/26.
+//
+
+import SwiftUI
+
+@main
+struct KOguruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
