41649e70ef76bcab5d3076c7f085ae33c15406d0
ulissesbon <ugoncalvesb@gmail.com>
2026-09-03 12:16:58 -0300

    feat: information card added to homeview

--- KOguru/Views/HomeView.swift
+++ KOguru/Views/HomeView.swift
@@ -9,48 +9,31 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        NavigationStack {
-            VStack{
+        ZStack{
+            Color(red: 0.18, green: 0.24, blue: 0.4)
+                .ignoresSafeArea()
+            
+            ScrollView(.vertical, showsIndicators: false){
                 VStack{
-                    VStack(alignment: .leading){
-                        Text("LOREN IPSUM")
-                            .font(.largeTitle)
-                            .bold()
-                        Text("consectetur adipiscing elit, sed do eiusmod tempor ")
+                    InformationHomeCard()
+                    
+                    Spacer()
+                    
+                    VStack(spacing: 16) {
+                        Text("VAMOS TREINAR")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .tracking(0.5)
                         
-                        
-                        
-                        Rectangle()
-                            .foregroundColor(.black)
-                            .frame(width: 177, height: 148)
-                            .cornerRadius(15)
+                        // Cards
+                        TrainCard(color: Color(red: 0.18, green: 0.24, blue: 0.4))
+                        TrainCard(color: Color(red: 0.3, green: 0.08, blue: 0.08))
+                        TrainCard(color: Color(red: 0.8, green: 0.66, blue: 0))
                         
                     }
-                    .padding()
+                    .padding(.horizontal, 16)
                 }
-                
-                VStack(spacing: 16) {
-    //                Text("LOREM IPSUM DOLOR SIT AMET?")
-    //                    .font(.title2)
-    //                    .bold()
-    //                    .foregroundColor(.black)
-    //                    .padding(.vertical, 28)
-                    
-                    // Cards
-                    Card(color: Color(red: 0.18, green: 0.24, blue: 0.4))
-                    Card(color: Color(red: 0.3, green: 0.08, blue: 0.08))
-                    Card(color: Color(red: 0.8, green: 0.66, blue: 0))
-                    
-                    
-                }
-                .frame(width: .infinity, height: .infinity)
-                .padding(.horizontal, 16)
-                
-                
             }
-            .frame(width: .infinity, height: .infinity)
-            .background(Color(red: 0.18, green: 0.24, blue: 0.4))
-            
         }
     }
 }
